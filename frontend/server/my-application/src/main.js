2431f47237b1a52a961291ab614119b4c94ce2eb
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2022-02-10 19:46:26 +0900

    main

--- frontend/server/my-application/src/main.js
+++ frontend/server/my-application/src/main.js
@@ -3,12 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueCookie from 'vue-cookie';
+import VueUploadComponent from "vue-upload-component"
 
 Vue.config.productionTip = false;
 
+Vue.component('file-upload', VueUploadComponent)
 new Vue({
   router,
   store,
   vuetify,
+  VueCookie,
   render: (h) => h(App),
 }).$mount("#app");
