412a520df80c3b658e320d061a197296308553cb
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2022-02-14 19:49:45 +0900

    add dev proxy settings

--- frontend/server/my-application/vue.config.js
+++ frontend/server/my-application/vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://172.17.0.1:8000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { "^/api/": "" }
+      },
+    }
+  }, 
   transpileDependencies: ["vuetify"],
 };
