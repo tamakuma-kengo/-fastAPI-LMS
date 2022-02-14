9a83b909267223591ac6d19c45ea9ea6e80c6fe5
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2022-02-14 19:49:19 +0900

    course page

---
+++ frontend/server/my-application/src/components/pages/Course.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-container>
+      {{this.username}} さん 
+      <v-row align="center" justify="space-around" v-if="is_creater">
+            <v-btn depressed color="primary" @click="add_course()" value="POST">
+                Add Course
+            </v-btn>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Course",
+  props: {
+    course_id: Number
+  },
+  created: function() {
+    let self = this
+    axios.get("http://127.0.0.1:8000/home_profile", {withCredentials: true})
+        .then(function(response){
+          console.log(response.data)
+          self.username = response.data.username
+          self.is_creater = response.data.create
+        }).catch(
+          function(error)  {
+            if(error.response.status == 401){
+              self.$router.push({name:'Signup'})
+            }else{
+              console.log(error.response)
+            }
+          }
+        )
+  },
+  data: () => ({
+    username : "",
+    is_creater : false,
+  }),
+  methods:{
+      add_course(){
+        this.$router.push({name:'RegisterCourse'})
+      }
+  },
+};
+</script>
